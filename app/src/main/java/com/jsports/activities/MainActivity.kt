d7007304916e4e353c0171e1f3ebc1ccd5e417d6
Eugene <ostashko4@gmail.com>
2019-12-15 15:50:42 +0200

    Main drawer commit

--- app/src/main/java/com/jsports/activities/MainActivity.kt
+++ app/src/main/java/com/jsports/activities/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.jsports.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.jsports.R
 import com.jsports.api.RetrofitClient
 import com.jsports.api.models.responses.BooleanResponse
@@ -10,15 +14,20 @@ import com.jsports.helpers.isAuthenticated
 import com.jsports.storage.SharedPrefManager
 import okhttp3.ResponseBody
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private var dl: DrawerLayout? = null
+    private var t: ActionBarDrawerToggle? = null
+    private var nv: NavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        isAuthenticated(this,object : RetrofitCallback<BooleanResponse>{
+        isAuthenticated(this, object : RetrofitCallback<BooleanResponse> {
             override fun onSuccess(value: BooleanResponse) {
-                if(!value.result){
+                if (!value.result) {
                     logout()
                 }
             }
@@ -31,9 +40,35 @@ class MainActivity : AppCompatActivity() {
 
         })
         title = getString(R.string.jsports_home)
+
+        dl = findViewById(R.id.activity_main)
+        t = ActionBarDrawerToggle(this, dl, R.string.open, R.string.close)
+
+        dl!!.addDrawerListener(t!!)
+        t!!.syncState()
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        nv = findViewById(R.id.nv)
+
+        nv!!.setNavigationItemSelectedListener(this)
     }
 
-    private fun logout(){
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return if (t!!.onOptionsItemSelected(item)) true else super.onOptionsItemSelected(item!!)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.profile -> {
+
+            }
+            else -> return true
+        }
+        return true
+    }
+
+    private fun logout() {
         SharedPrefManager.getInstance(this).clear()
         RetrofitClient.getInstance(this).api.logout()
     }
